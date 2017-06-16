775283b87ae4c6ecd12f9b337ee88a454646ed1f
xu <xuxiaoyi211@gmail.com>
2017-06-16 13:38:19 -0400

    twitch

---
+++ XuTwitch/XuTwitch/Main.cs
@@ -0,0 +1,89 @@
+﻿using HesaEngine.SDK;
+using HesaEngine.SDK.Enums;
+using HesaEngine.SDK.GameObjects;
+using XuTwitch.Modes;
+using static XuTwitch.SpellManager;
+using static XuTwitch.MenuManager;
+using static XuTwitch.DrawingManager;
+using static HesaEngine.SDK.Orbwalker;
+
+namespace XuTwitch
+{
+    public class Main : IScript
+    {
+        private readonly Champion champion = Champion.Twitch;
+
+        public string Name => "Xu Twitch" + champion;
+
+        public string Version => "0.0.1";
+
+        public string Author => "Xu211";
+
+        public AIHeroClient Player => ObjectManager.Player;
+        public static PredictionInput QPred, WPred, EPred, RPred;
+
+        public static OrbwalkerInstance Orbwalker => Core.Orbwalker;
+
+        public void OnInitialize()
+        {
+            Game.OnGameLoaded += Game_OnGameLoaded;
+        }
+
+        private void Game_OnGameLoaded()
+        {
+            if (Player.ChampionName != "Twitch")
+                return;
+
+            LoadMenu();
+
+            LoadSpells();
+
+            HesaEngine.SDK.AntiGapcloser.OnEnemyGapcloser += Modes.AntiGapcloser.DoAntigapclose;
+
+            LoadDrawings();
+
+            Game.OnTick += Game_OnTick;
+            Chat.Print(" Xu Twitch Loaded");
+        }
+
+
+
+        private void Game_OnTick()
+        {
+            var mana = Player.ManaPercent;
+
+            if (killstealMenu.GetCheckbox("enable"))
+            {
+                Killsteal.DoKS();
+            }
+
+
+            if (Orbwalker.ActiveMode == (OrbwalkingMode.Combo) && mana >= comboMenu.GetSlider("mana"))
+            {
+                Combo.DoCombo();
+            }
+
+            if (Orbwalker.ActiveMode == (OrbwalkingMode.Harass) && mana >= harassMenu.GetSlider("mana"))
+            {
+                Harass.DoHarass();
+            }
+
+            if (Orbwalker.ActiveMode == (OrbwalkingMode.LaneClear) && mana >= laneclearMenu.GetSlider("mana"))
+                LaneClear.DoLaneClear();
+
+            if (Orbwalker.ActiveMode == (OrbwalkingMode.LastHit) && mana >= lasthitMenu.GetSlider("mana"))
+                LastHit.DoLastHit();
+
+            if (Orbwalker.ActiveMode == (OrbwalkingMode.Flee))
+            {
+                Flee.DoFlee();
+            }
+
+        }
+
+        public static int GetEStackCount(Obj_AI_Base target)
+        {
+            return target.HasBuff("TwitchDeadlyVenom") ? target.GetBuffCount("TwitchDeadlyVenom") : 0;
+        }
+    }
+}
---
+++ XuTwitch/XuTwitch/Modes/Harass.cs
@@ -0,0 +1,35 @@
+﻿using HesaEngine.SDK;
+using static XuTwitch.MenuManager;
+using static XuTwitch.SpellManager;
+using static XuTwitch.Main;
+
+namespace XuTwitch.Modes
+{
+    public static class Harass
+    {
+        public static void DoHarass()
+        {
+            var w = harassMenu.GetCheckbox("useW") && W.IsReady();
+            var e = harassMenu.GetCheckbox("useE") && E.IsReady();
+            var estack = harassMenu.GetSlider("heStack");
+
+            var target = TargetSelector.GetTarget(Q.Range, TargetSelector.DamageType.Magical);
+            var rtarget = TargetSelector.GetTarget(R.Range, TargetSelector.DamageType.Magical);
+
+            if (target != null)
+            {
+                if (w && target.IsValidTarget(W.Range))
+                {
+                    W.PredictionCast(target, HitChance.High);
+                }
+                if (e && target.IsValidTarget(E.Range) && target.HasBuff("TwitchDeadlyVenom"))
+                {
+                    if (GetEStackCount(target) >= estack)
+                    {
+                        E.Cast();
+                    }
+                }
+            }
+        }
+    }
+}
